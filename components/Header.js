9176a6c3ba54fe5af29b4aca9237e4317416e85e
adyasha <adyashamohanty8105@gmail.com>
2021-05-21 13:15:26 +0530

    Chakra UI set up and add header

---
+++ components/Header.js
@@ -0,0 +1,74 @@
+
+import {
+  Box,
+  Flex,
+  Button,
+  Image,
+  useDisclosure
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { createBreakpoints } from "@chakra-ui/theme-tools"
+
+const breakpoints = createBreakpoints({
+  sm: "20em",
+  md: "48em",
+  lg: "62em",
+  xl: "80em",
+  "2xl": "96em",
+})
+
+const Header = (props) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const handleToggle = () => (isOpen ? onClose() : onOpen());
+
+  return (
+    <Flex
+      as="nav"
+      justify="space-between"
+      wrap="wrap"
+      padding={6}
+      bg="white"
+      color="white"
+      boxShadow="0 5px 10px rgb(154 160 185 / 5%), 0 15px 40px rgb(166 173 201 / 20%)"
+      pr={[5,18,50,120]}
+      pl={[5,18,50,120]}
+      {...props}
+    >
+      <Flex align="center" mr={5}>
+        <Box 
+        w={[100, 140, 170]}
+        objectFit="cover">
+          <Image src="./logo.svg" alt="Logo" />
+        </Box>
+      </Flex>
+
+      <Box display={{ base: "block", sm: "none" }} onClick={handleToggle}>
+        <HamburgerIcon w={6} h={6} m={2} color="black" />
+      </Box>
+
+      <Box
+        display={{ base: isOpen ? "block" : "none", sm: "block" }}
+        mt={{ base: 6, sm: 0 }}
+      >
+        <Button
+          bg="black"
+          variant="outline"
+          me={2}
+          _hover={{ opacity: "0.8", borderColor: "teal.700" }}
+        >
+          SIGN IN
+        </Button>
+        <Button
+          bg="black"
+          variant="outline"
+          me={2}
+          _hover={{ opacity: "0.8", borderColor: "teal.700" }}
+        >
+          SIGN UP
+        </Button>
+      </Box>
+    </Flex>
+  );
+};
+
+export default Header;
