c81e27e496c82d7352c6bd4587eac2ca9a0ee980
adyasha <adyashamohanty8105@gmail.com>
2021-05-22 13:34:19 +0530

    changes in Footer section(2)

--- components/Footer.js
+++ components/Footer.js
@@ -9,11 +9,11 @@ export default function Footer() {
     role="contentinfo"
     py="12"
     backgroundColor="#151414"
-    pr={[8,19,50,120]}
-    pl={[8,19,50,120]}
+    pr={[8,20,50,120]}
+    pl={[8,20,50,120]}
     px={8}
   >
-    <Stack spacing="10" divider={<StackDivider color="#878787" />}>
+    <Stack spacing="10" divider={<StackDivider />}>
       <Stack
         pb={10}
         direction={{
@@ -26,7 +26,7 @@ export default function Footer() {
         }}
       >
         <Box flex={1}
-        minW="250px">
+        minW="200px">
           <Image src="./logo.png" alt="Logo" />
         <Text color="#878787" pt={6} fontSize={{ base: "15px", md:"18px", xl: "22px" }} letterSpacing="1px">
         Making original truly unique
@@ -43,14 +43,14 @@ export default function Footer() {
           }}
         >
           <SimpleGrid 
-          columns={3}
+          columns={2}
           spacing={{
             base: '10',
             md: '20',
             lg: '28',
           }}
           flex="1">
-            <Box minW="300px" >
+            <Box minW="380px" >
               <Stack color="#878787" 
               fontSize={{ base: "15px", md:"18px", xl: "22px" }}
               >
@@ -59,7 +59,7 @@ export default function Footer() {
                 <Link>Our Collective</Link>
               </Stack>
             </Box>
-            <Box minW="300px"
+            <Box minW="380px"
             fontSize={{ base: "15px", md:"18px", xl: "22px" }}
             >
               <Stack color="#878787">
@@ -68,18 +68,29 @@ export default function Footer() {
                 <Link>Contact Us</Link>
               </Stack>
             </Box>
-            <Box Box minW="200px">
+            </SimpleGrid>
+            <Stack
+              direction={{
+                base: 'column',
+                md: 'row',
+              }}
+              spacing={{
+                base: '20',
+                md: '30',
+              }}
+            >
+            <Box flex="1">
             <Text color="#fff" pb={6} fontSize={{ base: "15px", md:"18px", xl: "22px" }} letterSpacing="1px" fontWeight="semibold">
               Follow <Text as="abbr" color="rgba(235,109,109)">Us</Text>
             </Text>
             <ButtonGroup variant="ghost" color="#878787">
-              <IconButton as="a" href="#" fontSize="2rem" mr={5} _hover={{backgroundColor: "rgba(235,109,109)", color: "#fff"}} icon={<FontAwesomeIcon icon={faTwitter} />} />
-              <IconButton as="a" href="#" fontSize="2rem" mr={5} _hover={{backgroundColor: "rgba(235,109,109)", color: "#fff"}} icon={<FontAwesomeIcon icon={faGithub} />} />
-              <IconButton as="a" href="#" fontSize="2rem" mr={5} _hover={{backgroundColor: "rgba(235,109,109)", color: "#fff"}} icon={<FontAwesomeIcon icon={faFacebook} />} />
-              <IconButton as="a" href="#" fontSize="2rem" _hover={{backgroundColor: "rgba(235,109,109)", color: "#fff"}} icon={<FontAwesomeIcon icon={faInstagram} />} />
+              <IconButton as="a" href="#" fontSize="1.8rem" mr={5} _hover={{backgroundColor: "rgba(235,109,109)", color: "#fff"}} icon={<FontAwesomeIcon icon={faTwitter} />} />
+              <IconButton as="a" href="#" fontSize="1.8rem" mr={5} _hover={{backgroundColor: "rgba(235,109,109)", color: "#fff"}} icon={<FontAwesomeIcon icon={faGithub} />} />
+              <IconButton as="a" href="#" fontSize="1.8rem" mr={5} _hover={{backgroundColor: "rgba(235,109,109)", color: "#fff"}} icon={<FontAwesomeIcon icon={faFacebook} />} />
+              <IconButton as="a" href="#" fontSize="1.8rem" _hover={{backgroundColor: "rgba(235,109,109)", color: "#fff"}} icon={<FontAwesomeIcon icon={faInstagram} />} />
             </ButtonGroup>
             </Box>
-          </SimpleGrid>
+            </Stack>
         </Stack>
       </Stack>
       <Stack
@@ -90,7 +101,7 @@ export default function Footer() {
         justifyContent="space-between"
         alignSelf="center"
       >
-          <Text color="#878787" fontSize={{ base: "15px", md:"18px", xl: "22px" }} >
+          <Text color="#878787" textAlign="center" fontSize={{ base: "15px", md:"18px", xl: "22px" }} >
             Copyright &copy; {new Date().getFullYear()} , All Rights Reserved <Text as="abbr" color="rgba(235,109,109)">Uncopied</Text>
           </Text>
       </Stack>
